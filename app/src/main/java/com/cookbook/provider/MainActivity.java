249cec6604b54141a4bc3a31eb9eee2c89fc552c
unknown <JZuurbier@C1359557.medewerkers.ad.hvu.nl>
2016-02-16 18:59:20 +0100

    Initial commit

---
+++ app/src/main/java/com/cookbook/provider/MainActivity.java
@@ -0,0 +1,41 @@
+package com.cookbook.provider;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	public static final Uri CONTENT_URI=
+			Uri.parse("content://com.cookbook.app.DiariesProvider/datastorage");
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ContentResolver resolver = getContentResolver();
+		Cursor cursor = resolver.query(CONTENT_URI, null, null, null, null);
+		TextView tv = (TextView) findViewById(R.id.textview1);
+		if (cursor.moveToFirst()){
+			while(true){
+				tv.append(cursor.getString(1) + "\r\n");
+				cursor.moveToNext();
+				if(cursor.isAfterLast()) break;
+			}
+		}
+		else
+			tv.append("no enties in diary");
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
